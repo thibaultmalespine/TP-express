3e312d5e106252d0ecebc3d71fadaf4f0718cdc1
thibaultmalespine <159419784+thibaultmalespine@users.noreply.github.com>
2024-09-24 13:53:59 +0200

    Delete models directory

--- models/restaurant.ts
+++
@@ -1,7 +0,0 @@
-const restaurant = {
-  name: "Fraternité Kebab",
-  description:
-    "Le meilleur kebab de la ville, avec des frites maison et une sauce secrète.",
-};
-
-export default restaurant;
