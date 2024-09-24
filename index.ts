ab31a05cf3498af686d349d3c79cf355399977cc
Votre Nom <Vous@exemple.com>
2024-09-24 15:37:19 +0200

    fin exercice 3

--- index.ts
+++ index.ts
@@ -6,6 +6,8 @@ import { engine } from "express-handlebars";
 
 const app = express();
 
+app.use(express.urlencoded({extended : false}));
+
 app.engine(
   "handlebars",
   engine({
@@ -23,16 +25,43 @@ app.get("/ping", (req, res) => {
 app.get("/", (req, res) => {
   res.render("home", {
     restaurant,
+    title : restaurant.name,
   });
 });
 
 app.get("/menus", (req, res) => {
   res.render("menu", {
     menus,
-    restaurant,
+    title : "Menus - "+restaurant.name,
   });
 });
 
+app.get("/commander", (req, res)=>{
+    const id = req.query.menu;
+    const menu = menus.find(menu => menu.id === id);
+    menu === undefined ? res.sendStatus(404) :
+    res.render("commande", {
+        menu,
+        title : `Commander - ${restaurant.name}`,
+    })
+})
+
+app.post('/commander', (req, res)=>{
+    const {name, address, phone, id} = req.body;
+    const person = {name, address, phone} ;
+    const menu = menus.find(menu => menu.id === id);
+    
+    res.render("commande", {
+        menu,
+        title : `Commander - ${restaurant.name}`,
+        person,
+    })
+})
+
 app.use("/assets", express.static(path.join(__dirname, "assets")));
+
+app.use('*', (req, res) => {
+    res.render("erreur")
+})
 
 app.listen(3000);
